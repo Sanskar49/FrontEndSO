494d05f419a73d014a8a891ab2ecf5a5ea821416
Sanskar B.C <sanskarbc14@gmail.com>
2021-05-27 23:29:50 +0545

    Routes made for HomePage and question ask page

--- src/App.js
+++ src/App.js
@@ -1,52 +1,49 @@
 import "./App.css";
 import { useState } from "react";
-import AddQuestion from "./components/AddQuestion";
-import AllQuestions from "./components/AllQuestions";
-
+import AddQuestion from "./components/Questions/AddQuestion";
+import AllQuestions from "./components/Questions/AllQuestions";
 import styled, { createGlobalStyle } from "styled-components";
-import Header from "./home/Header";
-import QuestionPage from "./home/QuestionPage";
+import Header from "./components/home/Header";
+import HomePage from "./components/home/HomePage";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   const GlobalStyles = createGlobalStyle` 
-
-   body {
+  body {
     background: #2d2d2d; 
     color: #fff;
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
    }
       
 `;
-
-  const [open, setOpen] = useState(false);
-
   const [questions, setQuestion] = useState([]);
-
   const addQuestion = async (task) => {
     const res = await fetch("http://localhost:8080/question/create", {
-      mode: "no-cors",
       method: "POST",
       headers: {
-        // "Accept" : "application/json, text/plain, */*",
         "Content-Type": "application/json",
       },
       body: JSON.stringify(task),
     });
 
     const data = await res.json();
-    console.log(data);
 
     setQuestion([...questions, data]);
   };
-
   return (
     <>
-      <div className="container">
-        <AddQuestion onAdd={addQuestion} />
-        {/* <GlobalStyles />
-      <Header />
-      <QuestionPage /> */}
-      </div>
+      <Router>
+        <GlobalStyles />
+        <Header />
+        <Switch>
+          <Route path="/" exact component={HomePage} />
+          <Route
+            path="/questions/ask"
+            exact
+            component={() => <AddQuestion onAdd={addQuestion} />}
+          />
+        </Switch>
+      </Router>
     </>
   );
 }
